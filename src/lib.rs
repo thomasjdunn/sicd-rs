7ca2a35526278d517d2f0bd8e8ed8f2b2cb46812
Vaughn Holmes <vaughnhlms@hotmail.com>
2023-10-22 20:54:52 -0400

    WIP: implement iamge data in sicd

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,21 @@
 //!
 //! It is a future goal to have functions for each version, but for now a single 
 //! function call and `match` statement are used. 
-use quick_xml::de::from_str;
-use serde::Deserialize;
 use std::fs::File;
 use std::path::Path;
+use std::slice::from_raw_parts;
 use std::str::{from_utf8, FromStr};
+
+use ndarray::{Array2, ArrayView2, par_azip};
+
 use thiserror::Error;
+use serde::Deserialize;
+use num_complex::Complex;
+use quick_xml::de::from_str;
 
 use nitf_rs::Nitf;
+use nitf_rs::segments::NitfSegment;
+use nitf_rs::headers::ImageHeader;
 
 pub mod dep;
 pub mod v1_3_0;
@@ -35,6 +42,43 @@ pub struct Sicd {
     pub meta: SicdMeta,
     /// SICD Version
     pub version: SicdVersion,
+    /// Image data from Nitf Image segements
+    pub image_data: Vec<ImageData>,
+    file: &'static File
+}
+
+/// Image data structure. Currently only implements Complex<f32> data type
+#[derive(Debug, Default)]
+pub struct ImageData {
+    byte_slice: &'static [u8],
+    new_size: usize, 
+    pub array: Array2<Complex<f32>>,
+    pub n_rows: usize,
+    pub n_cols: usize,
+}
+impl ImageData {
+    pub fn initialize(slice: &'static [u8], meta: &ImageHeader) -> Self {
+        let mut im_data = Self::default();
+        im_data.byte_slice = slice;
+        im_data.n_rows = meta.nrows.val as usize;
+        im_data.n_cols = meta.ncols.val as usize;
+        im_data.new_size = im_data.byte_slice.len() / std::mem::size_of::<Complex<f32>>();
+        let f32_ptr = im_data.byte_slice.as_ptr() as *const [[u8; 4]; 2]; // bit layout of complex number
+        let float_slice = unsafe { 
+            from_raw_parts(f32_ptr, im_data.new_size)
+        };
+        let aview = ArrayView2::from_shape(
+            (im_data.n_rows, im_data.n_cols), 
+            float_slice
+        ).unwrap();
+        im_data.array = Array2::zeros((im_data.n_rows, im_data.n_cols));
+        
+        par_azip!((out_elem in &mut im_data.array, in_elem in &aview) {
+            out_elem.re = f32::from_be_bytes(in_elem[0]);
+            out_elem.im = f32::from_be_bytes(in_elem[1]);
+        });
+        im_data
+    }
 }
 
 #[derive(Debug, Deserialize, PartialEq)]
@@ -118,7 +162,8 @@ impl SicdMeta {
 ///
 /// let sicd_path = Path::new("../example.nitf");
 /// let sicd = sicd_rs::read_sicd(sicd_path);
-/// // Then use `match` statement to extract metadata
+/// // Then use convenience methods provided by SicdMeta enum, or match off of version
+/// let meta = sicd.meta.get_v1_meta();
 /// ```
 ///   
 pub fn read_sicd(path: &Path) -> Sicd {
@@ -127,14 +172,24 @@ pub fn read_sicd(path: &Path) -> Sicd {
 }
 
 impl Sicd {
-    pub fn from_file(file: &mut File) -> Self {
+    pub fn from_file(file: &'static mut File) -> Self {
         let nitf = Nitf::from_file(file);
         let sicd_str = from_utf8(&nitf.data_extension_segments[0].data[..]).unwrap();
         let (version, meta) = parse_sicd(sicd_str).unwrap();
+        let n_img = nitf.nitf_header.meta.numi.val as usize;
+        let image_data: Vec<ImageData> = vec![];
+        for i_img in 0..n_img {
+            let tmp = ImageData::initialize(
+                &nitf.image_segments[i_img].data,
+                &nitf.image_segments[i_img].meta,
+            );
+            
+        }
         Self {
             nitf,
             meta,
             version,
+            image_data,
         }
     }
 }
